d2ca228d025e6d9532f12b66fb07304461979edc
Joel Hernandez <joel24hernan@gmail.com>
2019-06-12 22:07:22 -0500

    Initial commit. Have skeleton files.

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBPHMMON7JbhN4CNU0jOkBEWjuVnfOwGXs",
+    authDomain: "train-3d4b6.firebaseapp.com",
+    databaseURL: "https://train-3d4b6.firebaseio.com",
+    projectId: "train-3d4b6",
+    storageBucket: "train-3d4b6.appspot.com",
+    messagingSenderId: "853986962004",
+    appId: "1:853986962004:web:c80d7d73562e06f1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+ 
